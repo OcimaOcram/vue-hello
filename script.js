b87527f3cfb5641b1a2e33f36e267ecf5e6b91c6
OcimaOcram <marco.amico.pesci00@gmail.com>
2022-05-24 16:30:17 +0200

    Esercizio completo con bonus

---
+++ script.js
@@ -0,0 +1,8 @@
+const bungionissimo = new Vue({
+    element: "#root",
+    data: {
+        testo: "Ciao Mondo!",
+        img: "https://www.bgiorno.it/wp-content/uploads/2020/01/Buongiornissimo-3.jpg",
+        classi: "text-center"
+    },
+});
